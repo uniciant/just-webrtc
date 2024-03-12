e0ef912fabe212306ec73da27db45461f9e8f89d
Reece Kibble <reecek@uniciant.com>
2024-03-12 13:58:12 +0800

    examples/server/src/main.rs: utilize neshi keepalive

--- examples/server/src/main.rs
+++ examples/server/src/main.rs
@@ -1,9 +1,11 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use anyhow::Result;
 
 use neshi_server::{Nexus, self};
 use protocol::{NESHI_NATIVE_SERVER_ADDR, NESHI_WEB_SERVER_ADDR};
+
+const KEEPALIVE_INTERVAL: Duration = Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -12,8 +14,8 @@ async fn main() -> Result<()> {
     let nexus: Arc<Nexus> = Arc::from(Nexus::default());
     // run native and web servers concurrently
     tokio::select! {
-        result = neshi_server::serve_web(&nexus, NESHI_WEB_SERVER_ADDR) => result?,
-        result = neshi_server::serve(&nexus, NESHI_NATIVE_SERVER_ADDR) => result?,
+        result = neshi_server::serve_web(&nexus, NESHI_WEB_SERVER_ADDR, Some(KEEPALIVE_INTERVAL), None) => result?,
+        result = neshi_server::serve(&nexus, NESHI_NATIVE_SERVER_ADDR, Some(KEEPALIVE_INTERVAL), None) => result?,
     };
     Ok(())
 }
