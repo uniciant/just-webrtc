b02de7e5360e4740046e1b5aec939037a905b6bc
Reece Kibble <reecek@uniciant.com>
2024-03-28 18:11:24 +0800

    examples/signalling-server: init example signalling server binary

---
+++ examples/signalling-server/src/main.rs
@@ -0,0 +1,31 @@
+use std::{sync::Arc, time::Duration};
+
+use anyhow::Result;
+use just_webrtc_signalling::{server::Signalling, DEFAULT_NATIVE_SERVER_ADDR, DEFAULT_WEB_SERVER_ADDR};
+
+const KEEPALIVE_INTERVAL: Duration = Duration::from_secs(5);
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::try_init()?;
+
+    // create shared mapped signalling channels
+    let signalling = Arc::new(Signalling::new());
+    // create server futures
+    let serve_fut = just_webrtc_signalling::server::serve(
+        signalling.clone(),
+        DEFAULT_NATIVE_SERVER_ADDR.parse()?,
+        Some(KEEPALIVE_INTERVAL), None,
+        None,
+    );
+    let serve_web_fut = just_webrtc_signalling::server::serve_web(
+        signalling,
+        DEFAULT_WEB_SERVER_ADDR.parse()?,
+        Some(KEEPALIVE_INTERVAL), None,
+        None,
+    );
+    // run servers concurrently
+    tokio::try_join!(serve_fut, serve_web_fut)?;
+
+    Ok(())
+}
