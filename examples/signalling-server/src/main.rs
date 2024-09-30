3e3da9a5141edb38c0153f46f5e4dd4cd1e40168
reece <reecek@uniciant.com>
2024-09-30 14:59:35 +0800

    examples/signalling-server/src/main.rs: consistent import formatting

--- examples/signalling-server/src/main.rs
+++ examples/signalling-server/src/main.rs
@@ -1,9 +1,6 @@
-use std::{sync::Arc, time::Duration};
-
 use anyhow::Result;
-use just_webrtc_signalling::{
-    server::Signalling, DEFAULT_NATIVE_SERVER_ADDR, DEFAULT_WEB_SERVER_ADDR,
-};
+use just_webrtc_signalling::{server::Signalling, DEFAULT_NATIVE_SERVER_ADDR, DEFAULT_WEB_SERVER_ADDR};
+use std::{sync::Arc, time::Duration};
 
 const KEEPALIVE_INTERVAL: Duration = Duration::from_secs(5);
 
